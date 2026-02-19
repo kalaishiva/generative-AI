d4991dd401b0e7adb82c4381e9b63bed400157a7
designhappily <65686852+shivakalai@users.noreply.github.com>
2026-02-19 23:19:40 +0100

    Tokenization added

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import tiktoken
 # I want to create a token encoder for this particular model
 enc = tiktoken.encoding_for_model("gpt-4o")
 
+
+#text added
 text = "Hi there! My name is Kalaivani"
 tokens = enc.encode(text)
 
 print("Tokens : ", tokens)
 # output = Tokens [12194, 1354, 0, 3673, 1308, 382, 50151, 349, 3048]
+
 
 decoded = enc.decode([12194, 1354, 0, 3673, 1308, 382, 50151, 349, 3048])
 print("Decoded : ", decoded)
